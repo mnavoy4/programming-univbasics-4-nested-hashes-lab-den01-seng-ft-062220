c8ea2123f117380ab23b16ee9a7bbe4d46dd128a
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-21 18:19:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def adding_to_dennis
       }
     }
     
-    programmer_hash[:dennis_ritchie][:known_for].push("Assembly")
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
     programmer_hash
 
 end
